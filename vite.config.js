937fb1639431b87f8de83bed8afee04411bfcdf1
zxlee0114 <napoleon.lee0114@gmail.com>
2025-02-02 12:15:06 +0800

    fix: 修改路徑

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/hexschool-react-week3-assignment/"
+      : "/",
   plugins: [react()],
-})
+});
