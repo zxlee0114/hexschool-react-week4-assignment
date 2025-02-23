cdd4e524b0b0bed9c6679ab092be195885714d6c
zxlee0114 <napoleon.lee0114@gmail.com>
2025-02-23 21:45:23 +0800

    docs: 拆分出彈窗元件 (ProductModal.jsx)

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef, useCallback } from "react";
 import axios from "axios";
 import { Modal } from "bootstrap";
 import Pagination from "../components/pagination";
+import ProductModal from "../components/ProductModal";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const API_PATH = import.meta.env.VITE_API_PATH;
@@ -22,6 +23,7 @@ const defaultModalState = {
 
 function ProductPage({ setIsAuth }) {
   const [products, setProducts] = useState([]);
+  const [isProductModalOpen, setIsProductModalOpen] = useState(false);
 
   const getProducts = useCallback(
     async (page = 1) => {
@@ -81,16 +83,10 @@ function ProductPage({ setIsAuth }) {
     checkUserLogin();
   }, [checkUserLogin]);
 
-  const productModalRef = useRef(null);
   const delProductModalRef = useRef(null);
   const [modalMode, setModalMode] = useState(null);
 
   useEffect(() => {
-    new Modal(productModalRef.current, {
-      backdrop: false,
-    });
-    //console.log(Modal.getInstance(productModalRef.current));
-
     new Modal(delProductModalRef.current, {
       backdrop: false,
     });
@@ -112,14 +108,9 @@ function ProductPage({ setIsAuth }) {
         break;
     }
 
-    const modalInstance = Modal.getInstance(productModalRef.current);
-    modalInstance.show();
+    setIsProductModalOpen(true);
   };
 
-  const handleCloseProductModal = () => {
-    const modalInstance = Modal.getInstance(productModalRef.current);
-    modalInstance.hide();
-  };
   const handleOpenDelProductModal = (product) => {
     setTempProduct(product);
     const modalInstance = Modal.getInstance(delProductModalRef.current);
@@ -131,88 +122,6 @@ function ProductPage({ setIsAuth }) {
   };
 
   const [tempProduct, setTempProduct] = useState(defaultModalState);
-
-  const handleModalInputChange = (e) => {
-    const { name, value, checked, type } = e.target;
-    setTempProduct({
-      ...tempProduct,
-      [name]: type === "checkbox" ? checked : value,
-    });
-  };
-
-  const handleImageChange = (e, index) => {
-    const { value } = e.target;
-    const newImages = [...tempProduct.imagesUrl];
-    newImages[index] = value;
-    setTempProduct({
-      ...tempProduct,
-      imagesUrl: newImages,
-    });
-  };
-
-  const handleAddImage = () => {
-    const newImages = [...tempProduct.imagesUrl, ""];
-    setTempProduct({
-      ...tempProduct,
-      imagesUrl: newImages,
-    });
-  };
-
-  const handleRemoveImage = () => {
-    const newImages = [...tempProduct.imagesUrl];
-    newImages.pop();
-    setTempProduct({
-      ...tempProduct,
-      imagesUrl: newImages,
-    });
-  };
-
-  const updateProduct = async () => {
-    try {
-      await axios.put(
-        `${BASE_URL}/v2/api/${API_PATH}/admin/product/${tempProduct.id}`,
-        {
-          data: {
-            ...tempProduct,
-            origin_price: Number(tempProduct.origin_price),
-            price: Number(tempProduct.price),
-            is_enabled: tempProduct.is_enabled ? 1 : 0,
-          },
-        }
-      );
-    } catch (error) {
-      console.log(error);
-      alert("編輯產品失敗");
-    }
-  };
-
-  const createProduct = async () => {
-    try {
-      await axios.post(`${BASE_URL}/v2/api/${API_PATH}/admin/product`, {
-        data: {
-          ...tempProduct,
-          origin_price: Number(tempProduct.origin_price),
-          price: Number(tempProduct.price),
-          is_enabled: tempProduct.is_enabled ? 1 : 0,
-        },
-      });
-    } catch (error) {
-      console.log(error);
-      alert("新增產品失敗");
-    }
-  };
-
-  const handleUpdateProduct = async () => {
-    const apiCall = modalMode === "create" ? createProduct : updateProduct;
-    try {
-      await apiCall();
-      getProducts();
-      handleCloseProductModal();
-    } catch (error) {
-      console.log(error);
-      alert("更新產品失敗");
-    }
-  };
 
   const deleteProduct = async () => {
     try {
@@ -241,27 +150,6 @@ function ProductPage({ setIsAuth }) {
   // const handlePageChange = (page) => {
   //   getProducts(page);
   // };
-
-  const handleFileChange = async (e) => {
-    const file = e.target.files[0];
-    const formData = new FormData();
-    formData.append("file-to-upload", file);
-
-    try {
-      const res = await axios.post(
-        `${BASE_URL}/v2/api/${API_PATH}/admin/upload`,
-        formData
-      );
-
-      const uploadedImageUrl = res.data.imageUrl;
-      setTempProduct({
-        ...tempProduct,
-        imageUrl: uploadedImageUrl,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <>
@@ -334,257 +222,13 @@ function ProductPage({ setIsAuth }) {
         <Pagination pageInfo={pageInfo} getProducts={getProducts} />
       </div>
 
-      <div
-        ref={productModalRef}
-        id="productModal"
-        className="modal"
-        style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
-      >
-        <div className="modal-dialog modal-dialog-centered modal-xl">
-          <div className="modal-content border-0 shadow">
-            <div className="modal-header border-bottom">
-              <h5 className="modal-title fs-4">
-                {modalMode === "create" ? "新增產品" : "編輯產品"}
-              </h5>
-              <button
-                onClick={handleCloseProductModal}
-                type="button"
-                className="btn-close"
-                aria-label="Close"
-              ></button>
-            </div>
-
-            <div className="modal-body p-4">
-              <div className="row g-4">
-                <div className="col-md-4">
-                  <div className="mb-5">
-                    <label htmlFor="fileInput" className="form-label">
-                      圖片上傳
-                    </label>
-                    <input
-                      onChange={handleFileChange}
-                      type="file"
-                      accept=".jpg,.jpeg,.png"
-                      className="form-control"
-                      id="fileInput"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label htmlFor="primary-image" className="form-label">
-                      主圖
-                    </label>
-                    <div className="input-group">
-                      <input
-                        value={tempProduct.imageUrl}
-                        onChange={handleModalInputChange}
-                        name="imageUrl"
-                        type="text"
-                        id="primary-image"
-                        className="form-control"
-                        placeholder="請輸入圖片連結"
-                      />
-                    </div>
-                    <img
-                      src={tempProduct.imageUrl}
-                      alt={tempProduct.title}
-                      className="img-fluid"
-                    />
-                  </div>
-
-                  {/* 副圖 */}
-                  <div className="border border-2 border-dashed rounded-3 p-3">
-                    {tempProduct.imagesUrl?.map((image, index) => (
-                      <div key={index} className="mb-2">
-                        <label
-                          htmlFor={`imagesUrl-${index + 1}`}
-                          className="form-label"
-                        >
-                          副圖 {index + 1}
-                        </label>
-                        <input
-                          value={image}
-                          onChange={(e) => handleImageChange(e, index)}
-                          id={`imagesUrl-${index + 1}`}
-                          type="text"
-                          placeholder={`圖片網址 ${index + 1}`}
-                          className="form-control mb-2"
-                        />
-                        {image && (
-                          <img
-                            src={image}
-                            alt={`副圖 ${index + 1}`}
-                            className="img-fluid mb-2"
-                          />
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                  <div className="btn-group w-100">
-                    {tempProduct.imagesUrl.length < 5 &&
-                      tempProduct.imagesUrl[
-                        tempProduct.imagesUrl.length - 1
-                      ] !== "" && (
-                        <button
-                          onClick={handleAddImage}
-                          className="btn btn-outline-primary btn-sm w-100"
-                        >
-                          新增圖片
-                        </button>
-                      )}
-                    {tempProduct.imagesUrl.length > 1 && (
-                      <button
-                        onClick={handleRemoveImage}
-                        className="btn btn-outline-danger btn-sm w-100"
-                      >
-                        取消圖片
-                      </button>
-                    )}
-                  </div>
-                </div>
-
-                <div className="col-md-8">
-                  <div className="mb-3">
-                    <label htmlFor="title" className="form-label">
-                      標題
-                    </label>
-                    <input
-                      value={tempProduct.title}
-                      onChange={handleModalInputChange}
-                      name="title"
-                      id="title"
-                      type="text"
-                      className="form-control"
-                      placeholder="請輸入標題"
-                    />
-                  </div>
-
-                  <div className="mb-3">
-                    <label htmlFor="category" className="form-label">
-                      分類
-                    </label>
-                    <input
-                      value={tempProduct.category}
-                      onChange={handleModalInputChange}
-                      name="category"
-                      id="category"
-                      type="text"
-                      className="form-control"
-                      placeholder="請輸入分類"
-                    />
-                  </div>
-
-                  <div className="mb-3">
-                    <label htmlFor="unit" className="form-label">
-                      單位
-                    </label>
-                    <input
-                      value={tempProduct.unit}
-                      onChange={handleModalInputChange}
-                      name="unit"
-                      id="unit"
-                      type="text"
-                      className="form-control"
-                      placeholder="請輸入單位"
-                    />
-                  </div>
-
-                  <div className="row g-3 mb-3">
-                    <div className="col-6">
-                      <label htmlFor="origin_price" className="form-label">
-                        原價
-                      </label>
-                      <input
-                        value={tempProduct.origin_price}
-                        onChange={handleModalInputChange}
-                        name="origin_price"
-                        id="origin_price"
-                        type="number"
-                        className="form-control"
-                        placeholder="請輸入原價"
-                      />
-                    </div>
-                    <div className="col-6">
-                      <label htmlFor="price" className="form-label">
-                        售價
-                      </label>
-                      <input
-                        value={tempProduct.price}
-                        onChange={handleModalInputChange}
-                        name="price"
-                        id="price"
-                        type="number"
-                        className="form-control"
-                        placeholder="請輸入售價"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="mb-3">
-                    <label htmlFor="description" className="form-label">
-                      產品描述
-                    </label>
-                    <textarea
-                      value={tempProduct.description}
-                      onChange={handleModalInputChange}
-                      name="description"
-                      id="description"
-                      className="form-control"
-                      rows={4}
-                      placeholder="請輸入產品描述"
-                    ></textarea>
-                  </div>
-
-                  <div className="mb-3">
-                    <label htmlFor="content" className="form-label">
-                      說明內容
-                    </label>
-                    <textarea
-                      value={tempProduct.content}
-                      onChange={handleModalInputChange}
-                      name="content"
-                      id="content"
-                      className="form-control"
-                      rows={4}
-                      placeholder="請輸入說明內容"
-                    ></textarea>
-                  </div>
-
-                  <div className="form-check">
-                    <input
-                      checked={tempProduct.is_enabled}
-                      onChange={handleModalInputChange}
-                      name="is_enabled"
-                      type="checkbox"
-                      className="form-check-input"
-                      id="isEnabled"
-                    />
-                    <label className="form-check-label" htmlFor="isEnabled">
-                      是否啟用
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="modal-footer border-top bg-light">
-              <button
-                onClick={handleCloseProductModal}
-                type="button"
-                className="btn btn-secondary"
-              >
-                取消
-              </button>
-              <button
-                onClick={handleUpdateProduct}
-                type="button"
-                className="btn btn-primary"
-              >
-                確認
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ProductModal
+        modalMode={modalMode}
+        tempProduct={tempProduct}
+        getProducts={getProducts}
+        isOpen={isProductModalOpen}
+        setIsOpen={setIsProductModalOpen}
+      />
 
       <div
         ref={delProductModalRef}
